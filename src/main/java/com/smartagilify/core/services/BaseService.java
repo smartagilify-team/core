4a1c48b30ac42c55eb071143b200b81c28cc5e51
dehghani <dehghan7447@gmail.com>
2023-05-02 21:15:02 +0330

    dev(base): move mapper and business in service

--- src/main/java/com/smartagilify/core/services/BaseService.java
+++ src/main/java/com/smartagilify/core/services/BaseService.java
@@ -1,71 +1,113 @@
 package com.smartagilify.core.services;
 
 import com.smartagilify.core.entities.BaseEntity;
+import com.smartagilify.core.enumerations.EN_STATE;
+import com.smartagilify.core.exceptions.BusinessException;
+import com.smartagilify.core.mappers.BaseMapper;
+import com.smartagilify.core.model.BaseDTO;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
 @RequiredArgsConstructor
-public abstract class BaseService<E extends BaseEntity> {
+public abstract class BaseService<E extends BaseEntity, M extends BaseMapper<D, E>, D extends BaseDTO> {
 
-    protected final JpaRepository<E,Long> jpaRepository;
-    public <S extends E> S save(S entity){
-        return jpaRepository.save(entity);
+    protected final M mapper = Mappers.getMapper(getMapper());
+
+    protected abstract Class<M> getMapper();
+
+    protected final JpaRepository<E, Long> jpaRepository;
+
+    public D save(D dto, Long userId) {
+        E entity = mapper.dto2Entity(dto);
+        entity.setState(EN_STATE.CREATED);
+        entity.setCreateById(userId);
+        entity.setCreateDate(LocalDateTime.now());
+        return mapper.entity2Dto(jpaRepository.save(entity));
     }
 
-    public <S extends E> Iterable<S> saveAll(Iterable<S> entities){
-        return jpaRepository.saveAll(entities);
+    public D update(D dto, Long userId) {
+        E entity = mapper.dto2Entity(dto);
+        entity.setState(EN_STATE.UPDATED);
+        entity.setUpdateById(userId);
+        entity.setUpdateDate(LocalDateTime.now());
+        return mapper.entity2Dto(jpaRepository.save(entity));
     }
 
-    public Optional<E> findById(Long id){
-        return jpaRepository.findById(id);
+
+    public Iterable<D> saveAll(List<D> dtoList, Long userId) {
+        if (Objects.isNull(dtoList))
+            throw new BusinessException("Entities must not be null!");
+        List<D> out = new ArrayList<>();
+        for (D dto : dtoList) {
+            out.add(save(dto, userId));
+        }
+        return out;
     }
 
-    public List<E> findAll(){
-        return jpaRepository.findAll();
+    public D findById(Long id) {
+        Optional<E> byId = jpaRepository.findById(id);
+        return mapper.entity2Dto(byId.orElseThrow(() -> new BusinessException("ID IS NOT VALID")));
     }
 
-    public List<E> findAll(Sort sort){
-        return jpaRepository.findAll(sort);
+    public List<D> findAll() {
+        return mapper.entity2Dto(jpaRepository.findAll());
     }
 
-    public List<E> findAllById(Iterable<Long> ids) {
-        return jpaRepository.findAllById(ids);
+    public List<D> findAll(Sort sort) {
+        return mapper.entity2Dto(jpaRepository.findAll(sort));
+    }
+
+    public List<D> findAllById(Iterable<Long> ids) {
+        return mapper.entity2Dto(jpaRepository.findAllById(ids));
     }
 
     public Page<E> findAll(Pageable pageable) {
         return jpaRepository.findAll(pageable);
     }
 
-    public void deleteById(Long id){
+    public void softDelete(D dto, Long userId) {
+        E entity = mapper.dto2Entity(dto);
+        entity.setState(EN_STATE.DELETED);
+        entity.setUpdateById(userId);
+        entity.setUpdateDate(LocalDateTime.now());
+        jpaRepository.save(entity);
+    }
+
+    public void deleteById(Long id) {
         jpaRepository.deleteById(id);
     }
 
-    public void delete(E e){
-        jpaRepository.delete(e);
+    public void delete(D dto) {
+        jpaRepository.delete(mapper.dto2Entity(dto));
     }
 
-    public void deleteAllById(Iterable<Long> ids){
+    public void deleteAllById(Iterable<Long> ids) {
         jpaRepository.deleteAllById(ids);
     }
 
-    public void deleteAll(Iterable<? extends E> entities){
-        jpaRepository.deleteAll(entities);
+    public void deleteAll(List<D> dtoList) {
+        jpaRepository.deleteAll(mapper.dto2Entity(dtoList));
     }
 
-    public void deleteAllInBatch(Iterable<E> entities) {
-        jpaRepository.deleteAllInBatch(entities);
+    public void deleteAllInBatch(List<D> dtoList) {
+        jpaRepository.deleteAllInBatch(mapper.dto2Entity(dtoList));
     }
 
-    public void deleteAllByIdInBatch(Iterable<Long> ids){
+    public void deleteAllByIdInBatch(Iterable<Long> ids) {
         jpaRepository.deleteAllByIdInBatch(ids);
     }
 
-    public E getReferenceById(Long id){
+    public E getReferenceById(Long id) {
         return jpaRepository.getReferenceById(id);
     }
 
